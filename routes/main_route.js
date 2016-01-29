3c13c560f1bfdd7be56cccf259ece11c81efecd0
paragjyoti_bablu <paragjyoti2012@gmail.com>
2016-01-29 21:45:57 +0530

    main route added

--- routes/main_route.js
+++ routes/main_route.js
@@ -1,18 +1,23 @@
 var express = require('express');
+var mongoose= require('mongoose');
 var Post = mongoose.model('Post');
 var UserProfile = mongoose.model('UserProfile');
 var app=express.Router();
-var api_post = require('./api_post');
-var api_users =require('./api_users');
+var api_post = require('./post_controller');
+//var api_users =require('./api_users');
 
 //post api routes 
-app.post('/api/posts' , api_post.getAllPosts);
+
+app.post('/api/posts' , api_post.createPost);
+app.get('/api/posts' , api_post.getAllPosts);
 app.get('/api/posts/:id' , api_post.getPostById);
 app.put('/api/posts/:id' , api_post.updatePost);
 app.delete('/api/posts/:id' , api_post.deletePost);
 
-//uSER Api rotes
+/*//uSER Api rotes
 app.post('/api/users' , api_user.getAllUsers);
 app.get('/api/users/:id' , api_users.getUserById); 
 app.put('/api/users/:id' , api_users.updateUser);
 app.delete('/api/users/:id', api_users.deleteUser);
+*/
+module.exports=app;
---
+++ routes/post_controller.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var UserProfile = mongoose.model('UserProfile');
+
+
+exports.createPost  = function(req, res){
+
+		var post = new Post();
+		post.text = req.body.text;
+		post.created_by = req.body.created_by;
+		post.save(function(err, post){
+			if (err){
+				return res.send(500, err);
+			}
+			return res.json(post);
+		});
+};
+
+exports.getAllPosts =function(req, res){
+       Post.find(function(err, posts){
+			if(err){
+				return res.send(500, err);
+			}
+			return res.json(posts);
+		});
+	};
+    
+  exports.getPostById = function(req, res){
+		Post.findById(req.params.id, function(err, post){
+			if(err)
+				res.send(err);
+			res.json(post);
+		});
+	}; 
+	//updates specified post
+exports.updatePost= function(req, res){
+		Post.findById(req.params.id, function(err, post){
+			if(err)
+				res.send(err);
+
+			post.created_by = req.body.created_by;
+			post.text = req.body.text;
+
+			post.save(function(err, post){
+				if(err)
+					res.send(err);
+
+				res.json(post);
+			});
+		});
+	};
+    
+    //deletes the post
+exports.deletePost= function(req, res) {
+		Post.remove({
+			_id: req.params.id
+		}, function(err) {
+			if (err)
+				res.send(err);
+
+			res.json(Post);
+		 
+		});
+	};
